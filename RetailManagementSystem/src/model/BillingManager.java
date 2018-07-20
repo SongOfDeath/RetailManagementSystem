b91031dca9771ee323161abf05d1fb31c7dda14b
islom247 <islomiddin.sodiqov@gmail.com>
2018-07-20 23:38:15 +0500

    Update BillingManager.java

--- RetailManagementSystem/src/model/BillingManager.java
+++ RetailManagementSystem/src/model/BillingManager.java
@@ -1,5 +1,20 @@
 package model;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class BillingManager {
-
+    Arrayist<Bill> bills;
+    public BillingManager() {
+        bill = new ArrayList<>();
+    }
 }
