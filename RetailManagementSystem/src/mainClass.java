c9768d4e4f31af5a65c7fb40cc90b33069bb8a91
SongOfDeath <owningrazor@hotmail.com>
2018-07-20 15:21:50 +0300

    simple gui added

--- RetailManagementSystem/src/mainClass.java
+++ RetailManagementSystem/src/mainClass.java
@@ -1,13 +1,15 @@
 import javafx.application.*;
 import javafx.event.*;
 import javafx.scene.*;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.*;
-public class mainClass extends Application{
+public class mainClass extends Application implements EventHandler<ActionEvent>{
 	
+	Stage window;
+	Scene scene1, scene2;
 	Button button;
-	
 	public static void main(String[] args)
 	{
 		launch(args);
@@ -19,15 +21,35 @@ public class mainClass extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		primaryStage.setTitle("Shop System");
-		button = new Button("Submit");
+		window = primaryStage;
+		primaryStage.setTitle("Retail Management System");
 		
-		StackPane layout = new StackPane();
+		Label label1 = new Label("Welcome to retail management system");
+		
+		button = new Button("Submit");
+		button.setOnAction(this);
+		
+		VBox layout = new VBox();
+		TextField usernameField = new TextField("Username");
+		TextField passwordField = new TextField("Password");
+		//StackPane layout = new StackPane();
+		layout.getChildren().add(label1);
+		layout.getChildren().add(usernameField);
+		layout.getChildren().add(passwordField);
 		layout.getChildren().add(button);
 		
 		Scene scene = new Scene(layout, 500, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	@Override
+	public void handle(ActionEvent event)
+	{
+		if(event.getSource()==button)
+		{
+			System.out.println("submitted");
+		}
 	}
 	
 }
