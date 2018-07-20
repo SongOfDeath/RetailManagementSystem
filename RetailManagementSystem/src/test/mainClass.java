1a2fb8f0032dfb4a31394bb5c7376bebe2bc2d69
Doğacan <dogacan.kynak@gmail.com>
2018-07-20 16:53:14 +0300

    MVC design

---
+++ RetailManagementSystem/src/test/mainClass.java
@@ -0,0 +1,77 @@
+package test;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.*;
+import model.StockManager;
+import javafx.geometry.*;
+public class mainClass extends Application implements EventHandler<ActionEvent>{
+	
+	Stage window;
+	Scene scene1, scene2;
+	Button button;
+	TextField usernameField = new TextField("Username");
+	TextField passwordField = new TextField("Password");
+	ChoiceBox<String> shopTypeChoiceBox = new ChoiceBox<>();
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+		StockManager mg = new StockManager();
+		mg.addScore(5);
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		window = primaryStage;
+		primaryStage.setTitle("Retail Management System");
+		
+		Label welcomeLabel = new Label("Welcome to retail management system");
+		
+		button = new Button("Submit");
+		button.setOnAction(this);
+		
+		///////////////////////
+		shopTypeChoiceBox.getItems().add("Electronics");
+		shopTypeChoiceBox.getItems().add("Books");
+		shopTypeChoiceBox.getItems().add("Clothes");
+		///////////////////////
+		VBox layout = new VBox(10);
+		layout.setPadding(new Insets(20,20,20,20));
+
+		//StackPane layout = new StackPane();
+		layout.getChildren().add(welcomeLabel);
+		layout.getChildren().add(usernameField);
+		layout.getChildren().add(passwordField);
+		layout.getChildren().add(button);
+		layout.getChildren().add(shopTypeChoiceBox);
+		
+		//////////////
+		
+
+		
+		///////////
+		Scene scene = new Scene(layout, 500, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	@Override
+	public void handle(ActionEvent event)
+	{
+		if(event.getSource()==button)
+		{
+			System.out.println("submitted");
+			if(usernameField.getText()=="sercan")
+			{
+				//do something
+			}
+		}
+	}
+	
+}
